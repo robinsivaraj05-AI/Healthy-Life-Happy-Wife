fbc5698c815a754cf53d5dd45991964d1b706d90
Rabin Sivaraj <robinsivaraj05@gmail.com>
2026-07-18 02:06:02 +0400

    Simplify vite config - use default index.html entry

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: '.',
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: { main: 'index.html' }
-    }
+    outDir: 'dist'
   },
   server: {
-    port: 3000,
-    open: true
+    port: 3000
   }
 })
